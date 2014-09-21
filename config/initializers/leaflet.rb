968d164dbb702a556b50b83b206366d49fd21647
Tim Christensen <mail@timchristensen.de>
2014-09-21 17:00:31 +0200

    komisches Unicode-Zeichen

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,3 @@
 Leaflet.tile_layer = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-Leaflet.attribution = "© OpenStreetMap-Mitwirkende"
+Leaflet.attribution = "(c) OpenStreetMap-Mitwirkende"
 Leaflet.max_zoom = 19
