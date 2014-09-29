e845f1d1abb6742a0c2a9af12e99b1cb40c450bf
Irene <irene@nirgendwo.info>
2014-09-29 21:41:23 +0200

    Verkehrstraeger-Auswahl

--- config/initializers/global_params.rb
+++ config/initializers/global_params.rb
@@ -1,6 +1,6 @@
-VERKEHRSTRAEGER = {1 => "LKW", 2 => "Zug", 3 => "Schiff", 4 => "Flugzeug"}
+VERKEHRSTRAEGER = ["LKW", "Zug", "Schiff", "Flugzeug"]
 
-VERKEHRSTRAEGER_COMBOBOX =  Hash.new
-VERKEHRSTRAEGER.each do |key, kategorie|
-  VERKEHRSTRAEGER_COMBOBOX[kategorie] = key
+VERKEHRSTRAEGER_COMBOBOX =  []
+VERKEHRSTRAEGER.each do |kategorie|
+  VERKEHRSTRAEGER_COMBOBOX << [kategorie, kategorie]
 end
