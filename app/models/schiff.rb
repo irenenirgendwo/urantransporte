e84f206fe1e08dd3b1a8ea2f533f3c559a7102b9
Tim Christensen <mail@timchristensen.de>
2014-10-24 19:28:52 +0200

    Tabelle besser parsen

--- app/models/schiff.rb
+++ app/models/schiff.rb
@@ -37,51 +37,106 @@ class Schiff < ActiveRecord::Base
     end
   end
   
+  def self.parseTable(text)
+    master_column_position_list = []
+          
+    text.each_with_index do |line, line_number|
+      current_line_pos = 0
+      columns = line.strip.split(%r{ {2,}}).map{|col| col.strip}
+           
+      columns.each_with_index do | column, col_index |
+        col_start = line.index(column, current_line_pos)
+        current_line_pos = col_start + column.length
+        this_col_pos = [col_start, current_line_pos]
+           
+        if master_column_position_list.length == 0
+          master_column_position_list.push(this_col_pos)
+        else
+          master_column_position_list.each_with_index do | master_col_pos, m_index |
+            if master_col_pos[0] > this_col_pos[1]
+              master_column_position_list.insert(m_index, this_col_pos)
+              break
+            elsif master_col_pos[1] < this_col_pos[0]
+              if m_index == master_column_position_list.length-1
+                master_column_position_list.push(this_col_pos)
+                break
+              end # if m_index
+            elsif master_col_pos[0] <= this_col_pos[1] && master_col_pos[1] >= this_col_pos[0]
+              master_col_pos[0] = [master_col_pos[0], this_col_pos[0]].min 
+              master_col_pos[1] = [master_col_pos[1], this_col_pos[1]].max
+              break
+            end # of if master_col_pos[0] >
+          end # of iterating master_column_position_list
+        end # of if master_column_position_list.length ==
+              
+      end # of iterating columns
+          
+    end #of iterating temptext
+    p master_column_position_list
+
+    endtext = Array.new
+         
+    text.each_with_index do |line, line_number|
+      unless line.strip.blank?
+        endtext << master_column_position_list.map{|pos| line[(pos[0])..(pos[1])].to_s.strip}
+      end # of unless line.strip.blank?
+    end # of iterating temptext
+        
+    return endtext
+  end
+  
   def self.getMacsSchedule
     require 'open-uri'
     open("http://www.macship.com/macs-schedules/northcon.pdf", "rb") do |io|
       reader = PDF::Reader.new(io)
       reader.pages.each do |page|
-        
-        # Mehrdimensionales Array der Ankunftstabelle erzeugen
+           
+        # Spalten erkennen und mehrdimensionales Array der Ankunftstabelle erzeugen
         temptext = page.text.split("\n")
-        textend = temptext.map do |a1|
-          a1.split(%r{\s{2,}}) # Aufsplitten, wenn mindestens zwei Leerzeichen dazwischen sind.
+        deletetext = ["ETD", "ETA", "Schedule", "subject to change", "Please note"]
+        deletetext.each do |text|
+          temptext.delete_if {|item| item.include? text }
         end
+        
+        table = parseTable temptext
         
         # Alte Ankunftsdaten löschen
-        textend[3].each_with_index do |search1,i|
-          search2 = textend[5][i]
+        table[0].each_with_index do |search1,i|
+          search2 = table[1][i]
           unless search1.empty? || search2.empty?
             schiff = self.where('name LIKE ? AND name LIKE ?', "%#{search1}%", "%#{search2}%").first
-            self.update(schiff, next_ports: {})
+            if schiff
+              self.update(schiff, next_ports: {})
+            end
           end
         end
-        
+         
         # Neue Ankunftsdaten schreiben
         # Alle Zeilen durchgehen
-        textend.each do |line|
+        table.each do |line|
           hash = Hash.new
           # Alle Häfen durchgehen
           ports = ["Hamburg", "Rotterdam", "Vigo"]
           ports.each do |port|
             if line[0].to_s.include? port
               # Alle Spalten durchgehen
-              textend[3].each_with_index do |search1,i|
-                search2 = textend[5][i]
-                unless search1.empty? || search2.empty?
+              table[0].each_with_index do |search1,i|
+                search2 = table[1][i]
+                unless search1.empty? || search2.empty? || line[i].empty?
                   schiff = self.where('name LIKE ? AND name LIKE ?', "%#{search1}%", "%#{search2}%").first
-                  hash = schiff.next_ports
-                  hash[Date.parse line[i]] = port
-                  self.update(schiff, next_ports: hash)
+                  if schiff
+                    hash = schiff.next_ports
+                    hash[Date.parse line[i]] = port
+                    self.update(schiff, next_ports: hash)
+                  end
                 end
               end
             end
           end
         end
         
-      end
-    end
-  end
+      end #pages
+    end #Fileopen
+  end #getMacsSchedule
   
-end
+end #Class
