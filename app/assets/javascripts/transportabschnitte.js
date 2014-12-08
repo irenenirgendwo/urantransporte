cafc02556736eaebc4c01c9242e32c5fdbc16799
Irene <irene@nirgendwo.info>
2014-12-08 18:25:58 +0100

    transportabschnitte

---
+++ app/assets/javascripts/transportabschnitte.js
@@ -0,0 +1,8 @@
+function setup_start_anlagen_wahl(start_ort){ 
+  $('#transportabschnitt_start_ort').val(start_ort);
+}
+
+function setup_ziel_anlagen_wahl(start_ort){ 
+  $('#transportabschnitt_end_ort').val(start_ort);
+}
+
