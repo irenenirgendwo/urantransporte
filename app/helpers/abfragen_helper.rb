22afd4779562bef8acabb2d09635d5f0884da1db
Tim Christensen <mail@timchristensen.de>
2015-04-21 12:05:57 +0200

    Kalender-Darstellung

--- app/helpers/abfragen_helper.rb
+++ app/helpers/abfragen_helper.rb
@@ -4,4 +4,10 @@ module AbfragenHelper
   def calendar(date = Date.today.date, &block)
     Calendar.new(self, date, block).table date.year
   end
+  
+  def stringToColor(text)
+    require 'digest/md5'
+    color = '#' + Digest::MD5.hexdigest(text)[0..5]
+  end
+  
 end
