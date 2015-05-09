348c7078e24cfa0ce74361bebf0c8bdec8468ec0
Irene <irene@nirgendwo.info>
2015-05-09 22:33:04 +0200

    ort muss name haben

--- app/models/ort.rb
+++ app/models/ort.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 class Ort < ActiveRecord::Base
 
+  validates :name, presence: true
+
   has_and_belongs_to_many :transportabschnitte
   has_many :anlagen, :dependent => :restrict_with_error
   has_many :start_transportabschnitte, :foreign_key => 'start_ort_id', :class_name => "Transportabschnitt", :dependent => :restrict_with_error
