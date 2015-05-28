293f1e02dc7fc5e2c4c5ebf7b88461e46b98d44e
Tim Christensen <mail@timchristensen.de>
2015-05-28 23:50:38 +0200

    fix #33

--- app/assets/javascripts/setup_datetimepicker.js
+++ app/assets/javascripts/setup_datetimepicker.js
@@ -2,7 +2,7 @@ function setup_datetimepicker(name){
   $(name).datetimepicker({
     language: 'de'
   });
-  $(name).on("dp.change",function (e) {
-    $(name).data("DateTimePicker").setMaxDate(e.date);
-  });
+  $(name).click(function(event){
+   $(name).data("DateTimePicker").show();
+});
 }
