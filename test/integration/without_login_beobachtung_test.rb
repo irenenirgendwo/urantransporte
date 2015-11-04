dcb06301bf6fd5a2e25e6f98779503e813b5fde7
Irene <irene@nirgendwo.info>
2015-11-04 22:53:05 +0100

    Kommentierung Test zu #70

--- test/integration/without_login_beobachtung_test.rb
+++ test/integration/without_login_beobachtung_test.rb
@@ -18,22 +18,30 @@ class WithoutLoginBeobachtungTest < ActionDispatch::IntegrationTest
     assert_select '#mapedit', 1, "Ortsformular"
     assert_select '.panel', 7
     
-    # with Capybara
+    ## with Capybara, kommentiert fürs Nachmachen
+    
+    # Den Link besuchen
     visit '/'
+    # Überprüfen ob ein bestimmter Inhalt / Text auf der Seite ist
     assert page.has_content?("Beobachtung melden")
+    # Einen Link anklicken, gegeben werden muss der angezeigte Text des Links
     click_link "Beobachtung melden"
     assert page.has_content?("Beobachtung melden")
     assert page.has_content?("Richtung und Zeit")
     assert page.has_content?("Standort eintragen")
     
-    # Ohne eingegebene Daten funktioneirt das nicht
+    ## Ohne eingegebene Daten funktioneirt das nicht
+    # Button anklicken (manchmal sehen Links auch aus wie Buttons, im Zweifelsfall beides ausprobieren)
     click_button "Beobachtung erstellen"
     assert page.has_content?("Beobachtung melden")
     assert page.has_content?("muss ausgefüllt werden") || page.has_content?('Bitte einen Ort eingeben über die Karte oder das Namensfeld.')
 
-    # Dateneingabe und Beobachtungerstellung ohne Foto
+    ## Dateneingabe und Beobachtungerstellung ohne Foto
+    # Die id des html-input-Elements, das ausgefüllt werden soll und dann ein Hash :with => dem Wert, der da rein soll
     fill_in 'Fahrtrichtung des Transports', :with => 'Norden1'
     fill_in 'Ortsname', :with => 'Münster'
+    # Checkbox anhaken, wieder wird hier die id des input-Elements gebraucht,
+    # herauzubekommen über Firefox, auf die Seite gehen und Inspect Element / Untersuche Element
     check 'beobachtung_kennzeichen_radioaktiv'
     check 'beobachtung_polizei'
     click_button 'Beobachtung erstellen'
@@ -47,13 +55,17 @@ class WithoutLoginBeobachtungTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Vielen Dank für die Eingabe ihrer Beobachtung.')
     assert !page.has_content?("Foto hochladen") # muss falsch sein, has_content richtig?
     
-    #Angelegte Beobachtung pruefen
+    ## Angelegte Beobachtung pruefen
     beob = Beobachtung.find_by(fahrtrichtung: 'Norden1')
+    # prüft ob Wert (Beobachtung) nicht nil ist
     assert_not_nil beob
+    # Radioaktiv muss gesetzt sein, ätzend nicht (! ist Verneinung)
     assert beob.kennzeichen_radioaktiv
     assert !beob.kennzeichen_aetzend
     assert !beob.foto
+    # assert_equal prüft auf Gleichheit
     assert_equal "Formular", beob.quelle
+    # assert prüft immer, ob der Wert zu etwas, was als true verstanden wird ausgewertet wird
     assert beob.polizei
     
     # Neue Beobachtung, diesmal mit Foto
