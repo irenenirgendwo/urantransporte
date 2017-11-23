90e47a853ad3b9b762dd1e1c8a7d8ff3b0347fac
Irene <irene@nirgendwo.info>
2017-11-23 21:26:41 +0100

    logrotation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.logger = Logger.new(config.paths["log"].first, 3, 1.megabytes)
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
